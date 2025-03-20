e6a1068d3a4ddc1e16b9b3846fae3fdaca3a0f1d
ducws17 <ducvche170878@fpt.edu.vn>
2025-03-20 11:05:39 +0700

    admin page
    admin page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import Home from "./components/home";
+// import Home from "./components/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/forms/login";
-import Register from "./components/forms/register";
-import Detail from "./components/detail";
-import Cart from "./pages/cart/index";
-import Help from "./components/forms/help";
+// import Login from "./components/forms/login";
+// import Register from "./components/forms/register";
+// import Detail from "./components/detail";
+// import Cart from "./pages/cart/index";
+// import Help from "./components/forms/help";
 import Admin from "./components/admin/Admin";
-import DashBoard from "./components/admin/DashBoard/DashBoard";
-import ProductList from "./components/admin/Product/ProductList";
-import Userprofile from "./components/userprofile";
+// import DashBoard from "./components/admin/DashBoard/DashBoard";
+// import ProductList from "./components/admin/Product/ProductList";
+// import Userprofile from "./components/userprofile";
 import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/products/:id" element={<Detail />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/help" element={<Help />} />
+        <Route path="/help" element={<Help />} /> */}
         <Route path="/admin" element={<Admin />}>
-          <Route path="productList" element={<ProductList />} />
-          <Route path="dashboard" element={<DashBoard />} />
+          {/* <Route path="productList" element={<ProductList />} />
+          <Route path="dashboard" element={<DashBoard />} /> */}
         </Route>
-        <Route path="/profile/:id" element={<Userprofile />} />
+        {/* <Route path="/profile/:id" element={<Userprofile />} /> */}
       </Routes>
     </BrowserRouter>
   );
